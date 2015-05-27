94e4e91ca16bde158a960bdf68a7bc437a1e5289
RJWorth <rjw274@psu.edu>
2015-05-27 12:49:55 -0400

    constants module, copied from AlCen

---
+++ mks_constants.py
@@ -0,0 +1,21 @@
+
+
+
+### Define values for constants
+G			= 6.67384e-11	# m^3/kg/s^2 
+mSun		= 1.9891e30		# kg
+mEarth      = 5.972e24 		# kg
+AU			= 1.49597871e11	# m
+day			= 24*3600.		# s
+
+### Masses for AlphaCen system
+#mA, mB, mC = 1.105*mSun, 0.934*mSun, 0.123*mSun
+#m 			= [mA, mB, mC]
+#M 			= mA+mB+mC
+#mu 			= G*M
+
+### Conversion factors
+from math import pi
+deg2rad = 2*pi/360.
+rad2deg = 360./2*pi
+
