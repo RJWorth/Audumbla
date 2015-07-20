acb21ded0cd21629b96f3005d74dc2595f2e64d8
RJWorth <rjw274@psu.edu>
2015-07-20 15:00:34 -0400

    Added mass variance to disk generation

--- DiskRun.py
+++ DiskRun.py
@@ -2,78 +2,36 @@
 import Disks as d
 import numpy as np
 import matplotlib.pyplot as plt
-from cgs_constants import mSun,mEarth,AU,G
-
-rtr = 5.
-s   =10.
-rh  =10.
-dm  =1.e-7
-a   =1.5
-
+from cgs_constants import mSun,mEarth,mMoon,mMars,AU,G
+#-----------------------------------------------------------------------------#
+rtr = 5.    # truncation radius
+s   =10.    # coefficient of surface density (sigma = MMSN*s)
+rh  =10.    # number of hill radii counting as interaction
+m = [mMoon/mSun,mMars/mSun] # possible planetesimal masses
+f = [0.5, 0.5]    # fraction of total mass in each size
+a   =1.5    # alpha, slope of surface density profile
+ej  = .5    # fraction of interactions that should lead to ejection rather than collision
+#-----------------------------------------------------------------------------#
 reload(d)
 disk = d.Disk(r_out=rtr, alpha=a, sigC=s, rh=rh)
-disk.DebrisGenM(dm)
-len(disk.debris)
-#disk.DebrisGenA(da)
+disk.DebrisGenM(m,f)
+p, p_a, p_m, CorS = disk.debris.PltFormSuccessiveClearings(EjProb=ej)
 
-### RH/da ratio at center
-#p3 = [disk.debris]
-#mid = (len(p3[0])/2)
-#print( abs(p3[0][mid].a-p3[0][mid+1].a),p3[0][mid].RH2(p3[0][mid+1]), 
-#       abs(p3[0][mid].a-p3[0][mid+1].a)/p3[0][mid].RH2(p3[0][mid+1]) )
-
-pl ,pa,pm = disk.debris.PltFormTilClear(1000)
-pl1      = disk1.debris.PltFormTilClear(1000)
-
-p, p_a, p_m = disk.debris.PltFormSuccessiveClearings()
-#p1, p_a1, p_m1 = disk.debris.PltFormSuccessiveClearings()
-
-import cPickle as pickle
-pickle.dump(   p1, open(   "p1.p", "wb" ) )
-pickle.dump( p_a1, open( "p_a1.p", "wb" ) )
-pickle.dump( p_m1, open( "p_m1.p", "wb" ) )
-p1 = pickle.load( open( "p1.p", "rb" ) )
-
-
+### Plot
 for i in range(len(p_a)):
 	plt.plot([i for n in range(len(p_a[i]))], p_a[i], 'ro')
 
 plt.xlabel('# of Collisions')
 plt.ylabel('Semimajor Axis (AU)')
 plt.title('Planetesimal Merging Heuristic')
-plt.savefig('s{0}_r{1}_m{2}_a{3}-1.png'.format(s,rh,dm,a))
+plt.savefig('s{0}_r{1}_a{2}_ej{3}.png'.format(s,rh,a,ej))
 plt.clf()
 
-#plt.savefig('s{0}da{1}r10.png'.format(s,da))
-
-### How does RH2 change with a?
-r = [disk.debris[i].RH2(disk.debris[i+1]) for i in range(len(disk.debris)-1)]
-a = [disk.debris[i].a                     for i in range(len(disk.debris)-1)]
-plt.plot(a,r)
-plt.show()
-
-
-#reload(d)
-#disk = d.Disk(r_out=5.)
-#disk.DebrisGen(.1)
-#p1 = disk.debris.PltFormRand()
-
-#reload(d)
-#disk = d.Disk(r_out=5.)
-#disk.DebrisGen(.1)
-#p2 = disk.debris.PltFormTilClear()
-
-
-#for i in range(len(p)):
-#	for j in range(len(p[i].ListParams()[0])):
-#		plt.plot(i, p[i].ListParams()[0][j], 'ro')
-
-#plt.show()
-
-#for i in range(len(p2)):
-#	for j in range(len(p2[i].ListParams()[0])):
-#		plt.plot(i, p2[i].ListParams()[0][j], 'ro')
-
-#plt.show()
+### To save and reload arrays
+#import cPickle as pickle
+#pickle.dump(   p1, open(   "p1.p", "wb" ) )
+#pickle.dump( p_a1, open( "p_a1.p", "wb" ) )
+#pickle.dump( p_m1, open( "p_m1.p", "wb" ) )
+#p1 = pickle.load( open( "p1.p", "rb" ) )
 
 
