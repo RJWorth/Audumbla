e193efce6a13bae8a25e2ca2fbd6d8ea150dac4b
RJWorth <bot.collins@gmail.com>
2015-07-21 23:34:14 -0400

    Default dumpsteps 100x more often

--- Merc.py
+++ Merc.py
@@ -114,7 +114,7 @@ def WriteParamInFile(loc = 'Merc95/In/', f = 'in', alg='hybrid',
 	if (tOut == 'default'):
 		tOut = tf/1.e3
 	if (dtDump == 'default'):
-		dtDump = int(tf/dt/100.)
+		dtDump = int(tf/dt/10000.)
 
 	text = ''')O+_06 Integration parameters  (WARNING: Do not delete this line!!)
 ) Lines beginning with `)' are ignored.
